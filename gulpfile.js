3f812bb6ee7382b38dfb68d8535dd2861d941a6f
Carlton Joseph <carlton.joseph@gmail.com>
2016-09-22 11:17:35 -0700

    add location titles and fixed typo

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var yaml = require('yamljs');
+var swig = require('gulp-swig');
+var data = require('gulp-data');
 
+var yamlDir = '/home/carltonj2000/cj/configsNscripts/data/';
 gulp.task ('browser-sync', function () {
   browserSync.init({
     server: {
@@ -10,18 +13,35 @@ gulp.task ('browser-sync', function () {
   })
 });
 
+gulp.task('templates', function () {
+  return gulp.src('./templates/*.html')
+    .pipe(data(function () {
+      return require(yamlDir + 'passwords.json');
+      }))
+    .pipe(swig({defaults: {cache: false}})) // avoid caching for browser-sync
+    .pipe(gulp.dest('./'))
+});
+
 gulp.task ('watch',['browser-sync'], function () {
-  gulp.watch("*.html").on('change', browserSync.reload)
+  gulp.watch("index.html").on('change', browserSync.reload)
   gulp.watch("js/*.js").on('change', browserSync.reload)
   gulp.watch("css/*.css").on('change', browserSync.reload)
+  gulp.watch("templates/*.html", ['templates'])
 });
 
 
-gulp.task ('updateJson', function () {
-  yaml.load('/home/carltonj2000/cj/configsNscripts/data/vegasActivities.yml', function(result) {
+gulp.task ('updateActivities', function () {
+  yaml.load(yamlDir + 'vegasActivities.yml', function(result) {
     require('fs').writeFileSync(process.cwd() + '/vegasActivities.json',
       'var vegasActivities = ' + JSON.stringify(result, null, 2));
   });
 });
 
-gulp.task ('default', ['watch']);
+gulp.task ('updateKeys', function () {
+  yaml.load(yamlDir + 'passwords.yml', function(result) {
+    require('fs')
+      .writeFileSync(yamlDir + 'passwords.json',JSON.stringify(result, null, 2));
+  });
+});
+
+gulp.task ('default', ['templates','watch']);
